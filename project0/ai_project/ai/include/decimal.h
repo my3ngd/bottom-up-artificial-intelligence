eb70d52cc6cafa4c65c68662df8d69cc7b3a9ca4
2020098240 <2020098240@hanyang.ac.kr>
2021-12-23 13:52:19 +0900

    add: matrix type and operations definitions

--- project0/ai_project/ai/include/decimal.h
+++ project0/ai_project/ai/include/decimal.h
@@ -11,9 +11,11 @@ using std::deque;
 
 #include "gmpxx.h"
 
+// --------------------------------------------------------------------------------------------------------------------
+// real number
+
 // under 32 digit
 const uint16_t MAX_PRECISION = 32;
-
 
 class real_t
 {
@@ -112,5 +114,64 @@ real_t asinh(const real_t&);
 real_t acosh(const real_t&);
 real_t atanh(const real_t&);
 // */
+
+// --------------------------------------------------------------------------------------------------------------------
+// Matrices
+
+class matrix_t
+{
+private:
+    uint16_t rows;
+    uint16_t cols;
+    vector<vector<real_t>> data;
+
+public:
+    // constructors
+    matrix_t(const char&);
+    matrix_t(const uint16_t&, const uint16_t&);
+
+    // getters and setters
+    uint16_t get_rows(void) const;
+    uint16_t get_cols(void) const;
+
+    // unary operators
+    matrix_t operator-() const;
+
+    // binary operators
+    matrix_t operator+(const matrix_t&) const;
+    matrix_t operator-(const matrix_t&) const;
+    matrix_t operator*(const matrix_t&) const;
+    matrix_t operator*(const real_t&) const;
+    matrix_t operator/(const real_t&) const;
+
+    // binary friend operators
+    friend matrix_t operator*(const matrix_t&, const real_t&);
+    // no need to define friend operator/
+
+    // assignment operators
+    matrix_t& operator=(const matrix_t&);
+
+    matrix_t& operator+=(const matrix_t&);
+    matrix_t& operator-=(const matrix_t&);
+    matrix_t& operator*=(const matrix_t&);
+    matrix_t& operator*=(const real_t&);
+    matrix_t& operator/=(const real_t&);
+
+    // comparison
+    bool operator==(const matrix_t&) const;
+    bool operator!=(const matrix_t&) const;
+
+    // iostream
+    friend std::ostream& operator<<(std::ostream&, const matrix_t&);
+    friend std::istream& operator>>(std::istream&, matrix_t&);
+
+};
+
+// matrix operations
+matrix_t trans(const matrix_t&);
+matrix_t inverse(const matrix_t&);
+matrix_t pow(const matrix_t&, real_t);
+real_t det(const matrix_t&);
+real_t trace(const matrix_t&);
 
 #endif  // __MY3NGD_DECIMAL_H__
