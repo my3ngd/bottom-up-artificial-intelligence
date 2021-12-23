03781eda15c48a48d8132a964c08332e71a7ccc6
2020098240 <2020098240@hanyang.ac.kr>
2021-12-23 12:58:50 +0900

    add: gtest examples

--- project0/ai_project/test/basic_test.cc
+++ project0/ai_project/test/basic_test.cc
@@ -2,3 +2,33 @@
 #include <bits/stdc++.h>
 #include "decimal.h"
 
+#define FLAG 0
+
+TEST(SimpleTest, HowToUseGoogleTest)
+{
+    EXPECT_EQ(1, 2 - 1);
+    EXPECT_TRUE(true);
+    EXPECT_FALSE(false);
+}
+
+#if FLAG
+TEST(RealtSturctureTest, BigIntTest)
+{
+    real_t a;
+    real_t b;
+    a = string("9999999999999999999999999999999999999999999999");
+    b = 1;
+    real_t c("10000000000000000000000000000000000000000000000");
+
+    EXPECT_EQ(a + b, c);
+}
+
+TEST(RealtSturctureTest, SmallFloatTest)
+{
+    real_t a("0.00000000000000000000000000000001");
+    real_t b("0.00000000000000000000000000000002");
+    real_t c("0.00000000000000000000000000000016");
+    EXPECT_EQ(a*8 + 4*b, c);
+}
+
+#endif
