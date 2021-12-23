c8a6f0649e2f7a2e364a12371df110b07cc555ec
2020098240 <2020098240@hanyang.ac.kr>
2021-12-23 11:54:50 +0900

    GMP 컴파일 환경 설정

---
+++ project0/ai_project/ai/include/decimal.h
@@ -0,0 +1,23 @@
+#ifndef __MY3NGD_DECIMAL_H__
+#define __MY3NGD_DECIMAL_H__
+
+#include <bits/stdc++.h>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::deque;
+
+#include "gmpxx.h"
+
+class real_t
+{
+private:
+    mpz_class val;
+    uint16_t precision;
+public:
+};
+
+
+#endif  // __MY3NGD_DECIMAL_H__
