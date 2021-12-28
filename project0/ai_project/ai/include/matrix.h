f78ed039f1003d1a6a62f7e7aea7e1544bd9dd8f
my3ngd <hblee0402@gmail.com>
2021-12-28 13:42:15 +0900

    mod: remove 'const' to indexing operator[uint16_t]

--- project0/ai_project/ai/include/matrix.h
+++ project0/ai_project/ai/include/matrix.h
@@ -16,53 +16,53 @@ private:
 
 public:
     // constructors
-    matrix_t(const uint16_t&, const uint16_t&);
-    matrix_t(const uint16_t&, const uint16_t&, const char&);
+    matrix_t(uint16_t&, uint16_t&);
+    matrix_t(uint16_t&, uint16_t&, char&);
 
     // getters and setters
-    uint16_t get_rows(void) const;
-    uint16_t get_cols(void) const;
+    uint16_t get_rows(void);
+    uint16_t get_cols(void);
 
     // unary operators
-    matrix_t operator-() const;
+    matrix_t operator-();
 
     // binary operators
-    matrix_t operator+(const matrix_t&) const;
-    matrix_t operator-(const matrix_t&) const;
-    matrix_t operator*(const matrix_t&) const;
-    matrix_t operator/(const real_t&) const;
+    matrix_t operator+(matrix_t&);
+    matrix_t operator-(matrix_t&);
+    matrix_t operator*(matrix_t&);
+    matrix_t operator/(real_t&);
 
     // binary friend operators
-    friend matrix_t operator*(const matrix_t&, const real_t&);
+    friend matrix_t operator*(matrix_t&, real_t&);
     // no need to define friend operator/
 
     // assignment operators
-    matrix_t& operator=(const matrix_t&);
+    matrix_t& operator=(matrix_t&);
 
-    matrix_t& operator+=(const matrix_t&);
-    matrix_t& operator-=(const matrix_t&);
-    matrix_t& operator*=(const matrix_t&);
-    matrix_t& operator*=(const real_t&);
-    matrix_t& operator/=(const real_t&);
+    matrix_t& operator+=(matrix_t&);
+    matrix_t& operator-=(matrix_t&);
+    matrix_t& operator*=(matrix_t&);
+    matrix_t& operator*=(real_t&);
+    matrix_t& operator/=(real_t&);
 
     // comparison
-    bool operator==(const matrix_t&) const;
-    bool operator!=(const matrix_t&) const;
+    bool operator==(matrix_t&);
+    bool operator!=(matrix_t&);
 
     // indexing
-    vector<real_t>& operator[](const uint16_t&);
+    vector<real_t>& operator[](uint16_t&);
 
     // iostream
-    friend std::ostream& operator<<(std::ostream&, const matrix_t&);
+    friend std::ostream& operator<<(std::ostream&, matrix_t&);
     friend std::istream& operator>>(std::istream&, matrix_t&);
 
 };
 
 // matrix operations
-matrix_t trans(const matrix_t&);
-matrix_t inverse(const matrix_t&);
-matrix_t pow(const matrix_t&, real_t);
-real_t det(const matrix_t&);
-real_t trace(const matrix_t&);
+matrix_t trans(matrix_t&);
+matrix_t inverse(matrix_t&);
+matrix_t pow(matrix_t&, real_t);
+real_t det(matrix_t&);
+real_t trace(matrix_t&);
 
 #endif  // __MATRIX_H__
