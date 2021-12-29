50ebf02c96ba0d707859b21f78fb88cb2607a132
my3ngd <hblee0402@gmail.com>
2021-12-30 00:26:14 +0900

    add: class and enum

--- project1/ai_project/ai/include/neural_network.h
+++ project1/ai_project/ai/include/neural_network.h
@@ -3,4 +3,40 @@
 
 #include "matrix.h"
 
+// activation function
+enum ActivationFunction {
+    SIGMOID,
+    RELU,
+    // TANH,
+    // LEAKY_RELU,
+    // SOFTMAX,
+};
+
+
+matrix_t sigmoid(const matrix_t&);
+matrix_t relu(const matrix_t&);
+// matrix_t tanh(const matrix_t&);
+// matrix_t leaky_relu(const matrix_t&);
+// matrix_t softmax(const matrix_t&);
+
+
+// neural network class
+class neural_network
+{
+private:
+    vector<matrix_t> Weights;   // Weights of the neural network
+    vector<matrix_t> Biases;    // Biases of the neural network
+    vector<string> functions;   // Activation functions of the neural network
+public:
+    // constructors
+    neural_network(void);
+
+    // add layers
+    template<typename T>
+    void add_layer(T input_size, T output_size, ActivationFunction function);
+
+    // run the neural network
+    matrix_t run(matrix_t input);
+};
+
 #endif  // __NEURAL_NETWORK_H__
